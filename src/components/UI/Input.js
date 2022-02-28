65992ac6301a8f9442e0f646717e18fabacb698a
besimsherifi <sherifibesim@gmail.com>
2022-02-28 14:26:45 +0100

    commit:issue-2(context) #3 *added context files

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import classes from './Input.module.css'
 
 function Input(props) {
   return (
     <>
-        <div>
-            <label htmlFor="">{props.input.id}</label>
-            <input type="text" />
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input} />
         </div>
     </>
   )
